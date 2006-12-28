7ace33942d211fade23b2a4965a577dc4fb1930c
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2006-12-28 12:50:27 +0000

    HalTabCtrl.hpp about to be renamed.
    git-svn-id: svn://svn.geekisp.com/halite@112 362eace2-830a-0410-8e1a-3f32804a3aca

--- src/HalTabCtrl.hpp
+++ src/HalTabCtrl.hpp
@@ -5,9 +5,23 @@
 
 class CHalTabCtrl : public CWindowImpl<CHalTabCtrl, CTabCtrl>
 {
-public:
-	BEGIN_MSG_MAP(CHalTabCtrl)
-	END_MSG_MAP()
+public:	
 
+    BEGIN_MSG_MAP(CTabCtrlWithDisableImpl)
+//        MESSAGE_HANDLER(WM_CREATE, OnCreate)
+        DEFAULT_REFLECTION_HANDLER()
+    END_MSG_MAP()
 
+	void Attach(HWND hWndNew)
+	{
+		ATLASSERT(::IsWindow(hWndNew));
+        CWindowImpl<CHalTabCtrl, CTabCtrl>::SubclassWindow(hWndNew);
+	}
+	
+	LRESULT OnEraseBkgnd(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
+	{		
+		bHandled = TRUE;
+		return bHandled;	
+	}
+	
 };
