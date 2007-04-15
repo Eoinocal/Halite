efe0cbe69c7d0424aefb7565178df90d551e1692
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2007-04-15 17:28:44 +0000

    Forgot the Peers files.
    git-svn-id: svn://svn.geekisp.com/halite@182 362eace2-830a-0410-8e1a-3f32804a3aca

---
+++ src/advtabs/Peers.cpp
@@ -0,0 +1,49 @@
+﻿
+#include "../stdAfx.hpp"
+#include "../HaliteWindow.hpp"
+#include "../HaliteListView.hpp"
+
+//#include "../GlobalIni.hpp"
+//#include "../ini/Dialog.hpp"
+
+#include "../global/logger.hpp"
+
+#include "Peers.hpp"
+
+AdvPeerDialog::AdvPeerDialog(ui_signal& ui_sig, ListViewManager& single_sel) :
+	ui_(ui_sig),
+	selection_manager_(single_sel)
+{}
+
+void AdvPeerDialog::selectionChanged(const string& torrent_name)
+{	
+
+//	DoDataExchange(false);	
+	ui_.update();
+}
+
+void AdvPeerDialog::updateDialog()
+{}
+
+LRESULT AdvPeerDialog::onInitDialog(HWND, LPARAM)
+{
+	ui_.attach(bind(&AdvPeerDialog::updateDialog, this));
+	selection_manager_.attach(bind(&AdvPeerDialog::selectionChanged, this, _1));
+	
+	resizeClass::DlgResize_Init(false, true, WS_CLIPCHILDREN);
+	
+	m_list.Attach(GetDlgItem(IDC_PEERLIST));	
+	
+//	DoDataExchange(false);	
+	return 0;
+}
+
+void AdvPeerDialog::onClose()
+{
+	if(::IsWindow(m_hWnd)) 
+	{
+		::DestroyWindow(m_hWnd);
+	}
+}
+
+
---
+++ src/advtabs/Peers.hpp
@@ -0,0 +1,128 @@
+
+#pragma once
+
+#include "../stdAfx.hpp"
+#include "../global/string_conv.hpp"
+
+#include "../DdxEx.hpp"
+#include "../Halite.hpp"
+
+#include "../HaliteTabPage.hpp"
+#include "../HaliteListManager.hpp"
+
+class PeerListViewCtrl :
+	public CHaliteListViewCtrl<PeerListViewCtrl>,
+	public CHaliteIni<PeerListViewCtrl>,
+	private boost::noncopyable
+{
+	typedef PeerListViewCtrl thisClass;
+	typedef CHaliteIni<thisClass> iniClass;
+	typedef CHaliteListViewCtrl<thisClass> listClass;
+
+	friend class listClass;
+	
+public:
+	enum { 
+		LISTVIEW_ID_MENU = 0,
+		LISTVIEW_ID_COLUMNNAMES = HAL_DIALOGPEER_LISTVIEW_ADV	
+	};
+	
+	thisClass() :
+		iniClass("listviews/advPeers", "PeerListView")
+	{
+		array<int, 6> a = {{50, 50, 50, 50, 50, 50}};
+		SetDefaults(a);
+
+		load();
+	}
+
+	BEGIN_MSG_MAP_EX(thisClass)
+		MSG_WM_DESTROY(OnDestroy)
+
+		CHAIN_MSG_MAP(listClass)
+
+		DEFAULT_REFLECTION_HANDLER()
+	END_MSG_MAP()
+	
+	void updateListView()
+	{}
+	
+	void saveSettings()
+	{
+		GetListViewDetails();
+		save();
+	}
+
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version)
+    {
+		ar & boost::serialization::make_nvp("listview", boost::serialization::base_object<listClass>(*this));
+    }
+
+private:
+	void OnAttach()
+	{
+		SetListViewDetails();
+	}
+	
+	void OnDestroy()
+	{
+		saveSettings();
+	}
+};
+
+class ui_signal;
+
+typedef selection_manager<CHaliteListViewCtrl<HaliteListViewCtrl> > ListViewManager;
+
+class AdvPeerDialog :
+	public CHalTabPageImpl<AdvPeerDialog>,
+	public CDialogResize<AdvPeerDialog>//,
+//	public CWinDataExchangeEx<AdvPeerDialog>
+{
+protected:
+	typedef AdvPeerDialog thisClass;
+	typedef CHalTabPageImpl<thisClass> baseClass;
+	typedef CDialogResize<thisClass> resizeClass;
+
+public:
+	enum { IDD = IDD_ADVPEER };
+
+	AdvPeerDialog(ui_signal& ui_sig, ListViewManager& single_sel);
+
+	BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		return this->IsDialogMessage(pMsg);
+	}
+
+	BEGIN_MSG_MAP_EX(thisClass)
+		MSG_WM_INITDIALOG(onInitDialog)
+		MSG_WM_CLOSE(onClose)
+
+		if (uMsg == WM_FORWARDMSG)
+			if (PreTranslateMessage((LPMSG)lParam)) return TRUE;
+
+		CHAIN_MSG_MAP(resizeClass)
+		CHAIN_MSG_MAP(baseClass)
+		REFLECT_NOTIFICATIONS()
+	END_MSG_MAP()
+
+	BEGIN_DLGRESIZE_MAP(thisClass)
+		DLGRESIZE_CONTROL(IDC_PEERLIST, DLSZ_SIZE_X|DLSZ_SIZE_Y)
+	END_DLGRESIZE_MAP()
+
+	LRESULT onInitDialog(HWND, LPARAM);
+	void onClose();
+
+	LRESULT OnEditKillFocus(UINT uCode, int nCtrlID, HWND hwndCtrl);
+
+	void selectionChanged(const string& torrent_name);
+	void updateDialog();
+
+protected:
+	PeerListViewCtrl m_list;
+
+	ui_signal& ui_;
+	ListViewManager& selection_manager_;
+};
