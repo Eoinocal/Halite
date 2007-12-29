986785f6d6146fee0d65eef874f22f516d96cdb6
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2007-12-29 20:41:41 +0000

    Halite edit control seems to work for HaliteDialog and ConfigOptions. Not added to AdvTorrent yet.
    git-svn-id: svn://svn.geekisp.com/halite@366 362eace2-830a-0410-8e1a-3f32804a3aca

--- trunk/src/HaliteEditCtrl.hpp
+++ trunk/src/HaliteEditCtrl.hpp
@@ -1,29 +1,38 @@
 ﻿
-//         Copyright Eóin O'Callaghan 2006 - 2007.
+//         Copyright Eoin O'Callaghan 2006 - 2007.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once 
+
+#define WM_USER_HAL_EDITCHANGED	WM_USER + 100
+
 #include "stdAfx.hpp"
+#include <boost/logic/tribool.hpp>
 #include "global/string_conv.hpp"
 
 template<typename T>
-class CHalEditCtrl :
-	public CWindowImpl<CHalEditCtrl<T>, CEdit>,
+class CHaliteEditCtrl :
+	public CWindowImpl<CHaliteEditCtrl<T>, CEdit>,
 	private boost::noncopyable
 {
 protected:
-	typedef CHalEditCtrl<T> thisClass;
-	typedef CWindowImpl<CHalEditCtrl<T>, CEdit> baseClass;
+	typedef CHaliteEditCtrl<T> thisClass;
+	typedef CWindowImpl<thisClass, CEdit> baseClass;
 	
 public:
-	CHalEditCtrl(T lwr = -1, bool include = true) :
+	explicit CHaliteEditCtrl(T lwr = 0, bool include = false, bool signal = true) :
 		range_lwr_(lwr),
-		range_inc_(include)
+		range_inc_(include),
+		unapplied_(false),
+		signal_(signal)
 	{}
 
-    BEGIN_MSG_MAP_EX(CHalEditCtrl<T>)
-		REFLECTED_NOTIFY_CODE_HANDLER_EX(EN_KILLFOCUS, OnKillFocus)
+    BEGIN_MSG_MAP_EX(thisClass)
+		REFLECTED_COMMAND_CODE_HANDLER_EX(EN_KILLFOCUS, OnKillFocus)
+		REFLECTED_COMMAND_CODE_HANDLER_EX(EN_CHANGE, OnChange)
+		MSG_OCM_CTLCOLOREDIT(OnReflectedCtlColorEdit)
 		
         DEFAULT_REFLECTION_HANDLER()
     END_MSG_MAP()
@@ -34,41 +43,111 @@ public:
         baseClass::SubclassWindow(hWndNew);
 	}
 	
-	LRESULT OnKillFocus(LPNMHDR lpHdr)
-	{
+	LRESULT OnChange(UINT uNotifyCode, int nID, CWindow wndCtl)
+	{	
+		if (unapplied_)
+		{
+		}
+		else if (!unapplied_)
+		{
+			unapplied_ = true;
+			InvalidateRect(0, true);
+		}
+		else
+		{
+			unapplied_ = false;
+		}
+		
+		return 0;
+	}
+	
+	LRESULT OnKillFocus(UINT uNotifyCode, int nID, CWindow wndCtl)
+	{	
 		const int buffer_size = 512;
 		boost::array<wchar_t, buffer_size> buffer;
 		GetWindowText(buffer.elems, buffer_size);
 		
 		try
 		{
-		T val = lexical_cast<T>(buffer.elems);
-	
+		value_ = lexical_cast<T>(buffer.elems);
+			
 		if (range_inc_)
-			if (value_ < range_lwr_) val = -1;
+		{
+			if (value_ < range_lwr_) value_ = -102;
+		}
 		else
-			if (value_ <= range_lwr_) val = -1;
+		{
+			if (value_ <= range_lwr_) value_ = -103;
+		}
 			
 		}        
 		catch(boost::bad_lexical_cast &)
 		{
-		value_ = -1;
+		value_ = -101;
 		}
 		
-		if (value_ < 0)	SetWindowText(L"∞");
+		if (value_ < 0)	SetWindowText(hal::app().res_wstr(HAL_INF).c_str());
 		
-		SetMsgHandled(false);
+		if (signal_)
+		{
+			::SendMessage(GetParent(), WM_USER_HAL_EDITCHANGED, 0, 0);
+			unapplied_ = false;
+		}
 		
 		return 0;
 	}
 	
-	T Value() { return value_; }
+	HBRUSH OnReflectedCtlColorEdit(CDCHandle dc, CEdit edit)
+	{
+		if (signal_ && unapplied_)
+		{
+			SetTextColor(dc, RGB(0,0,255));
+			return GetSysColorBrush(COLOR_3DHILIGHT);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	T& Value() { return value_; }
+	
+	operator T& () { return Value(); }
+	
+	T& operator=(T val) 
+	{
+		if (!unapplied_) unapplied_ = boost::logic::indeterminate;
+		
+		if (range_inc_)
+		{
+			if (val < range_lwr_) val = -102;
+		}
+		else
+		{
+			if (val <= range_lwr_) val = -103;
+		}
+		
+		if (val < 0)	
+		{
+			value_ = -1;
+			SetWindowText(hal::app().res_wstr(HAL_INF).c_str());
+		}
+		else
+		{
+			value_ = val;
+			SetWindowText((lexical_cast<wstring>(value_)).c_str());
+		}
+		
+		return value_;
+	}		
 	
 private:
 	T value_;
 	
 	T range_lwr_;
 	bool range_inc_;
+	
+	boost::logic::tribool unapplied_;
+	bool signal_;
 };
 
-typedef CHalEditCtrl<int> CHalIntegerEditCtrl;
