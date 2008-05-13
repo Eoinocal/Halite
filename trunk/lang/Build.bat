419cda1a52171a0b6c01799f8598e81688a13104
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2008-05-13 21:18:27 +0000

    Moving updated language txts to root lang folder.
    git-svn-id: svn://svn.geekisp.com/halite@439 362eace2-830a-0410-8e1a-3f32804a3aca

--- trunk/lang/Build.bat
+++ trunk/lang/Build.bat
@@ -1,1 +1,1 @@
-call ruby tools\res_translator.rb ../res/ Halite Dutch French German Italian Japanese Norwegian NewLang Polish Portuguese Russian Serbian "Serbian (Cyrillic)" Spanish Swedish Turkish
+call ruby tools\res_translator.rb ../res/ Halite Dutch French German Italian Japanese Norwegian Template Polish Portuguese Russian Serbian "Serbian (Cyrillic)" Spanish Swedish Turkish
