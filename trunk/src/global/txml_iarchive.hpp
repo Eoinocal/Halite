f650f50e9b24f0eaea960b74f7847c1147fad209
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2008-08-16 20:56:58 +0000

    git-svn-id: svn://svn.geekisp.com/halite@549 362eace2-830a-0410-8e1a-3f32804a3aca

--- trunk/src/global/txml_iarchive.hpp
+++ trunk/src/global/txml_iarchive.hpp
@@ -285,7 +285,7 @@ public:
     template<class T>
     void load_override(const ::boost::serialization::nvp<T>& t, int)
 	{
-		TXML_LOG(boost::wformat(L" << loading: %1%") % t.name());
+		if (t.name()) TXML_LOG(boost::wformat(L" << loading: %1%") % t.name());
 
 		if (current_node_ && load_start(t.name()))
 		{
