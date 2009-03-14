8d6c99a87599c849bc43870619c554321c6acdab
Eoin <Eoin@362eace2-830a-0410-8e1a-3f32804a3aca>
2009-03-14 08:58:53 +0000

    git-svn-id: svn://svn.geekisp.com/halite/trunk@737 362eace2-830a-0410-8e1a-3f32804a3aca

--- src/halTorrentDefines.hpp
+++ src/halTorrentDefines.hpp
@@ -6,8 +6,8 @@
 
 #pragma once
 
-#define HALITE_VERSION							0, 3, 1, 734
-#define HALITE_VERSION_STRING					"v 0.3.1.7 dev 734"
+#define HALITE_VERSION							0, 3, 1, 735
+#define HALITE_VERSION_STRING					"v 0.3.1.7 dev 735"
 #define	HALITE_FINGERPRINT					"HL", 0, 3, 1, 7
 
 #ifndef HAL_NA
