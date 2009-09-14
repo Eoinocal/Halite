e3fcfdfe1279fb0e7b8381c0890adc4780059359
Eoin <eoin@binarynotions.com>
2009-09-14 18:01:17 +0100

    trunk folder deleted.
    Signed-off-by: Eoin <eoin@binarynotions.com>

--- trunk/lang/Build.bat
+++
@@ -1,3 +0,0 @@
-call ruby tools\res_translator.rb ../res/ Halite Czech Dutch French German Greek Hungarian Italian Japanese Norwegian Template Polish Portuguese Romanian Russian Serbian "Serbian (Cyrillic)" Spanish Slovenian Swedish Turkish 
-
-pause
