3ce3c7a3a54f3d5ce2a511707966282ab22bad17
unknown <C:\Users\Eoin\AppData\Roaming\The Bat!>
2009-10-07 20:10:25 +0100

    Release Candidate 2 version identifiers updated and released on sff.
    Signed-off-by:  <Eoin>

--- src/halTorrentDefines.hpp
+++ src/halTorrentDefines.hpp
@@ -7,7 +7,7 @@
 #pragma once
 
 #define HALITE_VERSION						0, 3, 2, 1075
-#define HALITE_VERSION_STRING				"v 0.3.2 RC2"
+#define HALITE_VERSION_STRING				"v 0.3.2 RC 2"
 #define HALITE_FINGERPRINT					"HL", 0, 3, 2, 0
 
 #ifndef HAL_NA
