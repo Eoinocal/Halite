828c6e2130028cbec11932447731c4dfe9713c64
Eoin <eoin@binarynotions.com>
2010-08-11 02:10:23 +0100

    Compiled new language files.
    Signed-off-by: Eoin <eoin@binarynotions.com>

--- lang/Build.bat
+++ lang/Build.bat
@@ -1,3 +1,3 @@
-call ruby tools\res_translator.rb ../res/ Halite Czech Dutch French German Greek Hungarian Italian Japanese Norwegian Polish Portuguese Romanian Russian Serbian "Serbian (Cyrillic)" Spanish Slovenian Swedish Turkish 
+call ruby tools\res_translator.rb ../res/ Halite Czech Dutch French German Greek Hungarian Italian Japanese "Macedonian(Cyrillic)" Norwegian Polish Portuguese Romanian Russian Serbian "Serbian(Cyrillic)" Slovenian Spanish Slovenian Swedish Turkish 
 
 pause
