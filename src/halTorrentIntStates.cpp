b35460eeb8ba1399fe3c5470671a1b51600fe5c4
Eoin <eoin@binarynotions.com>
2010-08-27 01:05:34 +0100

    Tiny tweak.
    Signed-off-by: Eoin <eoin@binarynotions.com>

--- src/halTorrentIntStates.cpp
+++ src/halTorrentIntStates.cpp
@@ -312,31 +312,32 @@ not_started::~not_started()
 
 sc::result not_started::react(const ev_start& evt)
 {
-	torrent_internal& t_i = context<torrent_internal>();
-	upg_lock_t l(t_i.mutex_);
+	{	torrent_internal& t_i = context<torrent_internal>();
+		upg_lock_t l(t_i.mutex_);
 
-	if (!t_i.info_memory(l) && !t_i.name_.empty())
-	{		
-		std::string torrent_info_file = 
-			to_utf8((hal::app().get_working_directory()/L"resume" / (t_i.name_ + L".torrent_info")).string());
-		string torrent_file = to_utf8((hal::app().get_working_directory()/L"torrents"/t_i.filename_).string());
+		if (!t_i.info_memory(l) && !t_i.name_.empty())
+		{		
+			std::string torrent_info_file = 
+				to_utf8((hal::app().get_working_directory()/L"resume" / (t_i.name_ + L".torrent_info")).string());
+			string torrent_file = to_utf8((hal::app().get_working_directory()/L"torrents"/t_i.filename_).string());
 
-		if (fs::exists(torrent_info_file))
-		{
-			HAL_DEV_MSG(L"Using torrent info data");
-			t_i.info_memory_.reset(new libt::torrent_info(torrent_info_file.c_str()));
+			if (fs::exists(torrent_info_file))
+			{
+				HAL_DEV_MSG(L"Using torrent info data");
+				t_i.info_memory_.reset(new libt::torrent_info(torrent_info_file.c_str()));
+			}
+			else if (fs::exists(torrent_file))
+			{
+				HAL_DEV_MSG(L"Using torrent file");
+				t_i.info_memory_.reset(new libt::torrent_info(torrent_file.c_str()));
+			}
 		}
-		else if (fs::exists(torrent_file))
-		{
-			HAL_DEV_MSG(L"Using torrent file");
-			t_i.info_memory_.reset(new libt::torrent_info(torrent_file.c_str()));
-		}
-	}
 
-	if (t_i.hash_str_.empty())
-	{
-		t_i.extract_hash(l);
-		t_i.update_manager(l);
+		if (t_i.hash_str_.empty())
+		{
+			t_i.extract_hash(l);
+			t_i.update_manager(l);
+		}
 	}
 
 	switch (stored_state_)
@@ -353,7 +354,6 @@ sc::result not_started::react(const ev_start& evt)
 	case torrent_details::torrent_stopped:
 	case torrent_details::torrent_stopping:		
 	case torrent_details::torrent_in_error:
-		l.unlock();
 		return transit< stopped >();
 	};
 
